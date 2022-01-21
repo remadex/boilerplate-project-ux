4cc75948ac0c6c4914fa2a6248a06497970f34fb
remadex <s.cardon@outlook.com>
2022-01-21 11:39:24 +0100

    add shop page for example

--- assets/js/templates/header.js
+++ assets/js/templates/header.js
@@ -1,13 +1,14 @@
 class MyHeader extends HTMLElement {
   connectedCallback() {
-    const page = this.getAttribute('current');
+    const current = this.getAttribute('current'); 
     this.innerHTML = `
       <header>
         <img src="" alt="">
         <nav>
           <ul>
-            <li class="${page === 'home' ? 'active' : ''}"><a href="index.html">Accueil</a></li>
-            <li class="${page === 'contact' ? 'active' : ''}"><a href="contact.html">Contact</a></li>
+            <li class="${current === 'home' ? 'active' : ''}"><a href="index.html">Accueil</a></li>
+            <li class="${current === 'contact' ? 'active' : ''}"><a href="contact.html">Contact</a></li>
+            <li class="${current === 'shop' ? 'active' : ''}"><a href="shop.html">Boutique</a></li>
           </ul>
         </nav>
       </header>
